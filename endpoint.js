3d2e4404aac142daca899e2ea8e4c240bbce8831
Goh Sheen An <sheen.an.goh@gmail.com>
2020-04-09 18:57:04 +0800

    Updates

---
+++ endpoint.js
@@ -0,0 +1,5 @@
+window.endpointConfig = {
+  buttons: [10, 38, -13, -18],
+  bars: [20, 250, 49],
+  limit: 100,
+};
---
+++ precache-manifest.f19d000e0683ac85247086ea9e83c362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a5cf0e20a51b6acc8fc2e44e583ed6",
+    "url": "/progress-bars/index.html"
+  },
+  {
+    "revision": "61819cef1a59fa8012ad",
+    "url": "/progress-bars/static/css/main.48e8f44a.chunk.css"
+  },
+  {
+    "revision": "27c6729e7d7188d2ec47",
+    "url": "/progress-bars/static/js/2.63ec2c0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/progress-bars/static/js/2.63ec2c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61819cef1a59fa8012ad",
+    "url": "/progress-bars/static/js/main.71fef068.chunk.js"
+  },
+  {
+    "revision": "3a13053dad8855823adb",
+    "url": "/progress-bars/static/js/runtime-main.d80717b0.js"
+  }
+]);
